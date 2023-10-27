fc42c29eb104faca8dd01718e32c89bf0fbb6bd0
vinayak501 <vinayakchavda2004@gmail.com>
2023-10-27 16:32:02 +0530

    models and data

---
+++ lib/models/grocery_item.dart
@@ -0,0 +1,10 @@
+import 'package:shopping_list/models/category.dart';
+
+class GroceryItem {
+  GroceryItem({required this.category,required this.id,required this.name,required this.quantity});
+
+  final String id;
+  final String name;
+  final int quantity;
+  final Category category;
+}
